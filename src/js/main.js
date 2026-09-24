5e4eb027c59860a69485013b3b106e355ce3aaae
joseph-tighe15 <565073@my.cuhsd.org>
2026-09-24 18:44:01 +0000

    has proper functionality

--- src/js/main.js
+++ src/js/main.js
@@ -5,23 +5,64 @@ img.addEventListener("load", () => {
   ctx.drawImage(img, 0, 0);
 });
 img.src = "src/assets/field.png"; // Set source path
-
+var paths = []
+colors = []
 const rect = c.getBoundingClientRect();
 const scaleX = c.width / rect.width;
 const scaleY = c.height / rect.height;
 var isPressing = false;
 c.addEventListener("mousedown", (e)=>{
-    ctx.moveTo((e.clientX-rect.left)*scaleX, (e.clientY-rect.top)*scaleY);
+    colors.push(ctx.strokeStyle);
+    paths.push([[(e.clientX-rect.left)*scaleX, (e.clientY-rect.top)*scaleY]])
+    ctx.beginPath();
+    ctx.moveTo(paths[paths.length-1][0][0], paths[paths.length-1][0][1]);
     isPressing = true;
 });
 c.addEventListener("mousemove", (e)=>{
     if (isPressing) {
-    ctx.lineTo((e.clientX-rect.left)*scaleX, (e.clientY-rect.top)*scaleY);
+        paths[paths.length-1].push([(e.clientX-rect.left)*scaleX, (e.clientY-rect.top)*scaleY])
+        ctx.lineTo(paths[paths.length-1][paths[paths.length-1].length-1][0], paths[paths.length-1][paths[paths.length-1].length-1][1]);
+        ctx.stroke();
     }
 });
 c.addEventListener("mouseup", (e)=>{
     isPressing = false;
-    ctx.lineTo((e.clientX-rect.left)*scaleX, (e.clientY-rect.top)*scaleY);
+    paths[paths.length-1].push([(e.clientX-rect.left)*scaleX, (e.clientY-rect.top)*scaleY])
+    ctx.lineTo(paths[paths.length-1][paths[paths.length-1].length-1][0], paths[paths.length-1][paths[paths.length-1].length-1][1]);
     ctx.stroke();
 });
 ctx.strokeStyle = "#ff00ff"
+document.getElementById("lineColor").value = "#ff00ff"
+document.getElementById("lineColor").addEventListener("change", (e)=>{
+    ctx.strokeStyle = document.getElementById("lineColor").value;
+});
+function reset() {
+    ctx.fillStyle = "white";
+    ctx.rect(0, 0, c.width, c.height);
+    ctx.fill();
+    ctx.drawImage(img, 0, 0);
+}
+document.getElementById("reset").addEventListener("click", reset);
+
+document.getElementById("back-one").addEventListener("click", (e)=>{
+    reset();
+    paths.pop()
+    var i = 1;
+    for (let path of paths) {
+        ctx.strokeStyle = colors[i];
+        ctx.beginPath();
+        ctx.moveTo(path[0][0], path[0][1]);
+        for (let dot of path) {
+            ctx.lineTo(dot[0], dot[1]);
+            ctx.stroke();
+        }
+        i++;
+    }
+    ctx.strokeStyle = document.getElementById("lineColor").value;
+});
+document.addEventListener('keydown', function(event) {
+  if ((event.ctrlKey || event.metaKey) && event.key.toLowerCase() === 'z') {
+    event.preventDefault(); 
+    document.getElementById("back-one").click();
+  }
+});
